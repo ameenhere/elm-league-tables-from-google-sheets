1310bb848b53f8cf74b00d0a789b986906b97711
Ameen <ameen@dhis2.org>
2021-07-04 20:52:29 +0200

    minor

--- src/Calculations/LeagueTableFromLeagueGames.elm
+++ src/Calculations/LeagueTableFromLeagueGames.elm
@@ -223,10 +223,10 @@ homePoints : DecodedGame -> Int
 homePoints game =
     case ( game.homeTeamGoalCount, game.awayTeamGoalCount ) of
         ( Just homeTeamGoalCount, Just awayTeamGoalCount ) ->
-            if homeTeamGoalCount > awayTeamGoalCount && ((homeTeamGoalCount == 6 && awayTeamGoalCount<5) || (homeTeamGoalCount == 7 && awayTeamGoalCount<6))  then
+            if (homeTeamGoalCount > awayTeamGoalCount && ((homeTeamGoalCount == 6 && awayTeamGoalCount<5) || (homeTeamGoalCount == 7 && awayTeamGoalCount<6)))  then
                 3
 
-            else if homeTeamGoalCount < awayTeamGoalCount && ((awayTeamGoalCount == 6 && homeTeamGoalCount<5) || (awayTeamGoalCount == 7 && homeTeamGoalCount<6)) then
+            else if (homeTeamGoalCount < awayTeamGoalCount && ((awayTeamGoalCount == 6 && homeTeamGoalCount<5) || (awayTeamGoalCount == 7 && homeTeamGoalCount<6))) then
                 0
             
             else if homeTeamGoalCount > awayTeamGoalCount    then
@@ -243,10 +243,10 @@ awayPoints : DecodedGame -> Int
 awayPoints game =
     case ( game.homeTeamGoalCount, game.awayTeamGoalCount ) of
         ( Just homeTeamGoalCount, Just awayTeamGoalCount ) ->
-            if homeTeamGoalCount > awayTeamGoalCount && ((homeTeamGoalCount == 6 && awayTeamGoalCount<5) || (homeTeamGoalCount == 7 && awayTeamGoalCount<6))  then
+            if (homeTeamGoalCount > awayTeamGoalCount && ((homeTeamGoalCount == 6 && awayTeamGoalCount<5) || (homeTeamGoalCount == 7 && awayTeamGoalCount<6)))  then
                 0
 
-            else if homeTeamGoalCount < awayTeamGoalCount && ((awayTeamGoalCount == 6 && homeTeamGoalCount<5) || (awayTeamGoalCount == 7 && homeTeamGoalCount<6)) then
+            else if (homeTeamGoalCount < awayTeamGoalCount && ((awayTeamGoalCount == 6 && homeTeamGoalCount<5) || (awayTeamGoalCount == 7 && homeTeamGoalCount<6))) then
                 3
             
             else if homeTeamGoalCount > awayTeamGoalCount    then
